3c67e4b456260530d031e9ad4470d138f5a9c200
masras2006 <masras2006@outlook.com>
2017-06-16 14:58:11 -0700

    initial setup

---
+++ CroweHorwath.Client.Console/Program.cs
@@ -0,0 +1,17 @@
+﻿using CroweHorwath.Api;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CroweHorwath.Client.Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Console.ReadLine();
+        }
+    }
+}
